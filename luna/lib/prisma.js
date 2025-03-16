8f63fe61df198bcb7abf6913a2d08abc3695fc19
MU AHMED <lifeofcoder19.com>
2025-03-16 17:47:52 +0530

    all

--- luna/lib/prisma.js
+++ luna/lib/prisma.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db=new PrismaClient();
+export const db=globalThis.prisma || new PrismaClient();
 if (process.env.Node_ENV !=="production"){
     globalThis.prisma = db;
     
